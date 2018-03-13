18d159783685f322037cd789d844053b28fa7d43
bittencourtthulio <bittencourtthulio@gmail.com>
2018-03-13 09:17:14 -0300

    Conference

---
+++ Embarcadero Conference 2017/UX_UI/Unit3.pas
@@ -0,0 +1,25 @@
+unit Unit3;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.WebBrowser;
+
+type
+  TForm3 = class(TForm)
+    WebBrowser1: TWebBrowser;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.fmx}
+
+end.
